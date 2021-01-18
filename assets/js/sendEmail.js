08bf306e03bbbb4619b42fa31b26d3e12cbff672
Dalec2013 <dalec2013@gmail.com>
2021-01-18 11:53:49 +0000

    Implement emailjs intergration on contact page

---
+++ assets/js/sendEmail.js
@@ -0,0 +1,15 @@
+function sendEmail(contactForm){
+    sendEmailjs.send("gmail", "dale", {
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.emailaddress.value,
+        "project_request": contactForm.projectsummary.value
+    })
+    .then(
+        function (response){
+            console.log("SUCCESS", response);
+        },
+        function (error){
+            console.log("FAILED", error);
+        });
+        
+}
